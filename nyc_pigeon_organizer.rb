221912194271f63e9640ac94b92bcd0bb09598b1
patrick simmons <hadroncollides@gmail.com>
2021-01-28 01:39:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,8 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  # convert :color :purple to new hash as keys.
+  binding.pry
+  nyc_birds = data.each_with_object({}) do |(key,value|), ending_array|
 end
